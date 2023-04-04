2c23bd0be0f42afba102522d7be5eb105f77862b
Abdul Kadir <akadir@wcgclinical.com>
2023-04-04 17:45:48 -0500

    Update environment.prod.ts

--- MyApp/src/environments/environment.prod.ts
+++ MyApp/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
+  checkinUrl: 'https://localhost:7231/checkin',
+  checkoutUrl: 'https://localhost:7231/checkout',
+  reportUrl: 'https://localhost:7231/api/Dashboard',
   production: true
 };
